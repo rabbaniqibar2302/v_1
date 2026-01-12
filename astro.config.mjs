7bdc6063969942eb67059974fd28ceb7a6251ef7
qibar2302 <rizkiakbar2302@gmail.com>
2026-01-12 14:21:47 +0700

    phase 1

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import mdx from "@astrojs/mdx";
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [mdx()],
+});
---
+++ src/assets/note.md
@@ -0,0 +1,2 @@
+1. masing masing konten di isi dengan menggunakan mdx
+2. jadi buat dulu layout masing masing dari konten, jadi nambah mdx, nambah konten
